97b56fab0c179e8f0a0d3357afe6248a34bdc2aa
DESKTOP-HSLA09I\Ryan <spocksbeard16@gmail.com>
2021-03-03 16:45:53 -0400

    changing to updated google analytics

--- web/gatsby-config.js
+++ web/gatsby-config.js
@@ -11,9 +11,19 @@ const isProd = process.env.NODE_ENV === "production";
 module.exports = {
   plugins: [
     {
-      resolve: `gatsby-plugin-google-analytics`,
+      resolve: `gatsby-plugin-google-gtag`,
       options: {
-        trackingId: process.env.GOGGLE_ANALYTICS,
+        // You can add multiple tracking ids and a pageview event will be fired for all of them.
+        trackingIds: [
+          process.env.GOOGLE_ANALYTICS, // Google Analytics / GA
+        ],
+        // This object gets passed directly to the gtag config command
+        // This config will be shared across all trackingIds
+        gtagConfig: {
+          optimize_id: "OPT_CONTAINER_ID",
+          anonymize_ip: true,
+          cookie_expires: 0,
+        },
       },
     },
     "gatsby-plugin-postcss",
